ec72db4b83caaf87691b9381f8d5236644205bd8
theonelog <awni.alquraini@gmail.com>
2024-09-02 08:14:31 -0700

    making the program more functional

--- RomFS File Comparisson/romfscomp/src/main/java/com/log/Main.java
+++ RomFS File Comparisson/romfscomp/src/main/java/com/log/Main.java
@@ -3,18 +3,38 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 
 public class Main {
     public static void main(String[] args) {
       FileCompare manager = new FileCompare();
-      manager.compareVersions("D:\\Console Games\\RomFS\\Splatoon 3 7.2.0 FS", "D:\\Console Games\\RomFS\\Splatoon 3 9.0.0 FS");
+      //manager.compareVersions("D:\\Console Games\\RomFS\\Splatoon 3 7.2.0 FS", "D:\\Console Games\\RomFS\\Splatoon 3 9.0.0 FS");
     }
     
 }
 
 class FileCompare{
+    private String name;
+    private String version;
+    private File filterList;
+    private boolean listExists;
 
+    public FileCompare(){
+        Scanner s = new Scanner(System.in);
+        System.out.print("What is the name of the game you are comparing file versions of?: ");
+        name = s.nextLine();
+        System.out.print("What is the current version of that game?: ");
+        version = s.nextLine();
+        System.out.print("Do you have an already existing filter list .txt file?(yes or no): ");
+        String yon = s.nextLine();
+        if(yon.equals("yes")){
+            System.out.print("what is the directory of the file?(without the quotation marks on the end): ");
+            filterList = new File(s.nextLine());
+            listExists = true;
+        }
+        s.close();
+    }
     private void getFiles(String path, ArrayList<String> nameArrayList){
         File folder = new File(path);
         File[] files = folder.listFiles();
@@ -28,37 +48,44 @@ class FileCompare{
         } 
     }
     public void compareVersions(String oldVerDir, String newVerDir){
-        ArrayList<String> oldVerNames = new ArrayList<>();
-        getFiles(oldVerDir, oldVerNames);
         ArrayList<String> newVerNames = new ArrayList<>();
         getFiles(newVerDir, newVerNames);
-        for(int i = 0; i < oldVerNames.size(); i++){
-            for(int j = 0; j < newVerNames.size(); j++){
-                if(oldVerNames.get(i).equals(newVerNames.get(j))){
-                    newVerNames.remove(j);
-                    j--;
+        if(!listExists){
+            ArrayList<String> oldVerNames = new ArrayList<>();
+            getFiles(oldVerDir, oldVerNames);
+            for(int i = 0; i < oldVerNames.size(); i++){
+                for(int j = 0; j < newVerNames.size(); j++){
+                    if(oldVerNames.get(i).equals(newVerNames.get(j))){
+                        newVerNames.remove(j);
+                        j--;
+                    }
                 }
+            }
+        }
+        else{
+            Scanner reader = new Scanner(filterList); //Need to make the Scanner read thru an already existing filter list that is stored in the main file of the program. Use this link
+            //https://search.brave.com/search?q=how+to+parse+through+a+txt+file+in+java
+            while (reader.hasNextLine()){
+
             }
         }
         writeToFile(newVerNames);
     }
     private void writeToFile(ArrayList<String> fNames){
         try {
-            File exists = new File("list.txt");
+            File exists = new File(name + " " + version + " RomFS differences.txt");
             if(exists.createNewFile()){
-                FileWriter updater = new FileWriter("list.txt");
+                FileWriter updater = new FileWriter(name + " " + version + " RomFS differences.txt");
                 for(String fName : fNames){
-                    updater.write(fName);
-                    updater.write("\n");
+                    updater.write(fName + "\n");
                 }
                 updater.close();
             }
             else{
-                new FileWriter("list.txt", false).close();
-                FileWriter updater = new FileWriter("list.txt");
+                new FileWriter(name + " " + version + " RomFS differences.txt", false).close();
+                FileWriter updater = new FileWriter(name + " " + version + " RomFS differences.txt");
                 for(String fName : fNames){
-                    updater.write(fName);
-                    updater.write("\n");
+                    updater.write(fName + "\n");
                 }
                 updater.close();
             }
